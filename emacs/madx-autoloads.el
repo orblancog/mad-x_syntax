1107f583b0cba1e1ca7ebde72428f23cc76eb405
orblancog <orblancog@gmail.com>
2017-11-18 20:19:07 +0100

    moving comments in  autoload

--- emacs/madx-autoloads.el
+++ emacs/madx-autoloads.el
@@ -1,7 +1,8 @@
 ;;; madx-autoloads.el --- loads madx-mode
-(global-font-lock-mode t);; Enable syntax highlighting
+(global-font-lock-mode t); Enable syntax highlighting
 (setq font-lock-maximum-decoration t)
-;(add-to-list 'load-path "~/.emacs.d"); edit accordingly to madx.el location
+;; edit accordingly to madx.el location
+;(add-to-list 'load-path "~/.emacs.d")
 (autoload 'madx-mode "madx" "madx-mode" t)
 (setq auto-mode-alist (append '(("\\.madx$" . madx-mode))
    auto-mode-alist))
